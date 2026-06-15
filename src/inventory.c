328c979bed447fd8a2cab605693c6dce7154712d
BijouTOP <guilhermeroquesousa@protonmail.com>
2026-06-15 03:09:52 +0100

    mimir we cooked we need to rush this or pay an indian bro

--- src/inventory.c
+++ src/inventory.c
@@ -58,10 +58,14 @@ char itemModel[50] = "";
 char itemIp[16] = "";
 char itemMac[18] = "";
 char itemLocation[100] = "";
-char itemStatus[20] = "";
 char itemLastMaintenanceDate[11] = "";
+
 bool editModes[9] = {false};
-int activeItems[2] = {0};
+int activeType = {0};
+int activeStatus = {0};
+int MactiveType = {0};
+int MactiveStatus = {0};
+
 bool showAddItemDialog = false;
 Node *inventoryList = NULL;
 
@@ -70,32 +74,58 @@ Rectangle viewItems = {0};
 
 Vector2 scrollDialog = {0, 0};
 Rectangle viewDialog = {0};
-void saveItem()
+
+bool isEditing = false;
+Node *nodeBeingEdited = NULL;
+void saveItem(char name[50], char brand[50], char model[50], char ip[16], char mac[18], char location[100], char maintenanceDate[11], int *type, int *status)
 {
-    Node *newNode = (Node *)malloc(sizeof(Node));
+    if (isEditing && nodeBeingEdited != NULL)
+    {
+        strncpy(nodeBeingEdited->item.name, name, 50);
+        strncpy(nodeBeingEdited->item.brand, brand, 50);
+        strncpy(nodeBeingEdited->item.model, model, 50);
+        strncpy(nodeBeingEdited->item.ipAddress, ip, 16);
+        strncpy(nodeBeingEdited->item.macAddress, mac, 18);
+        strncpy(nodeBeingEdited->item.location, location, 100);
+        strncpy(nodeBeingEdited->item.lastMaintenanceDate, maintenanceDate, 11);
 
-    strncpy(newNode->item.name, itemName, 50);
-    strncpy(newNode->item.brand, itemBrand, 50);
-    strncpy(newNode->item.model, itemModel, 50);
-    strncpy(newNode->item.ipAddress, itemIp, 16);
-    strncpy(newNode->item.macAddress, itemMac, 18);
-    strncpy(newNode->item.location, itemLocation, 100);
+        nodeBeingEdited->item.type = (EquipmentType)(*type);
+        nodeBeingEdited->item.status = (EquipmentStatus)(*status);
 
-    newNode->item.type = (EquipmentType)activeItems[0];
-    newNode->item.status = (EquipmentStatus)activeItems[1];
+        isEditing = false;
+        nodeBeingEdited = NULL;
+    }
+    else
+    {
+        Node *newNode = (Node *)malloc(sizeof(Node));
 
-    newNode->next = inventoryList;
-    inventoryList = newNode;
+        strncpy(newNode->item.name, name, 50);
+        strncpy(newNode->item.brand, brand, 50);
+        strncpy(newNode->item.model, model, 50);
+        strncpy(newNode->item.ipAddress, ip, 16);
+        strncpy(newNode->item.macAddress, mac, 18);
+        strncpy(newNode->item.location, location, 100);
+        strncpy(newNode->item.lastMaintenanceDate, maintenanceDate, 11);
 
-    itemName[0] = '\0';
-    itemBrand[0] = '\0';
-    itemModel[0] = '\0';
-    itemIp[0] = '\0';
-    itemMac[0] = '\0';
-    itemLocation[0] = '\0';
-    activeItems[0] = 0;
-    activeItems[1] = 0;
-    showAddItemDialog = false;
+        newNode->item.type = (EquipmentType)(*type);
+        newNode->item.status = (EquipmentStatus)(*status);
+
+        newNode->next = inventoryList;
+        inventoryList = newNode;
+
+        showAddItemDialog = false;
+
+        itemName[0] = '\0';
+        itemBrand[0] = '\0';
+        itemModel[0] = '\0';
+        itemIp[0] = '\0';
+        itemMac[0] = '\0';
+        itemLocation[0] = '\0';
+        itemLastMaintenanceDate[0] = '\0';
+
+        activeType = 0;
+        activeStatus = 0;
+    }
 }
 void drawTextBoxWithPlaceholder(Rectangle bounds, char *text, int textSize, bool *editMode, const char *placeholder)
 {
@@ -106,6 +136,106 @@ void drawTextBoxWithPlaceholder(Rectangle bounds, char *text, int textSize, bool
     if (GuiTextBox(bounds, text, textSize, *editMode))
     {
         *editMode = !(*editMode);
+    }
+}
+
+void showModal(char itemName[50], char itemBrand[50], char itemModel[50], char itemIp[16], char itemMac[18], char itemLocation[100], char itemLastMaintenanceDate[11], int *activeType, int *activeStatus)
+{
+    Rectangle dialogBounds = {GetScreenWidth() / 4, GetScreenHeight() / 4, GetScreenWidth() / 2, GetScreenHeight() / 2};
+    Rectangle contentDialog = {0, 0, dialogBounds.width - 20, dialogBounds.height < 355 ? 500 : dialogBounds.height - 32};
+    GuiScrollPanel(dialogBounds, isEditing ? "Modificar Item" : "Adicionar Item", contentDialog, &scrollDialog, &viewDialog);
+
+    BeginScissorMode(dialogBounds.x, dialogBounds.y + 24, dialogBounds.width, dialogBounds.height - 32);
+
+    const char *placeholders[7] = {"Nome", "Marca", "Modelo", "IP", "MAC", "Localização", "Ultima Verificação"};
+
+    for (int i = 0; i < 10; i++)
+    {
+        Rectangle textBoxBounds = {dialogBounds.x + 20, dialogBounds.y + 40 + (i * 45) + scrollDialog.y, dialogBounds.width - 60, 35};
+
+        switch (i)
+        {
+        case 0:
+            drawTextBoxWithPlaceholder(textBoxBounds, itemName, 50, &editModes[0], placeholders[0]);
+            break;
+        case 1:
+            drawTextBoxWithPlaceholder(textBoxBounds, itemBrand, 50, &editModes[1], placeholders[1]);
+            break;
+        case 2:
+            break;
+        case 3:
+            if (editModes[2])
+            {
+                break;
+            }
+            drawTextBoxWithPlaceholder(textBoxBounds, itemModel, 50, &editModes[3], placeholders[2]);
+            break;
+        case 4:
+            if (editModes[2])
+            {
+                break;
+            }
+            drawTextBoxWithPlaceholder(textBoxBounds, itemIp, 16, &editModes[4], placeholders[3]);
+            break;
+        case 5:
+            if (editModes[2])
+            {
+                break;
+            }
+            drawTextBoxWithPlaceholder(textBoxBounds, itemMac, 18, &editModes[5], placeholders[4]);
+            break;
+        case 6:
+            if (editModes[2])
+            {
+                break;
+            }
+            drawTextBoxWithPlaceholder(textBoxBounds, itemLocation, 100, &editModes[6], placeholders[5]);
+            break;
+        case 7:
+            break;
+        case 8:
+            if (editModes[2] || editModes[7])
+            {
+                break;
+            }
+            drawTextBoxWithPlaceholder(textBoxBounds, itemLastMaintenanceDate, 11, &editModes[8], placeholders[6]);
+            break;
+        case 9:
+            if (editModes[2] || editModes[7])
+            {
+                break;
+            }
+            if (GuiLabelButton(textBoxBounds, isEditing ? "Modificar Item" : "Adicionar Item"))
+            {
+                saveItem(itemName, itemBrand, itemModel, itemIp, itemMac, itemLocation, itemLastMaintenanceDate, activeType, activeStatus);
+            }
+            break;
+        }
+    }
+    if (editModes[2])
+    {
+        EndScissorMode();
+    }
+    Rectangle dropdownTypeBounds = {dialogBounds.x + 20, dialogBounds.y + 40 + (2 * 45) + scrollDialog.y, dialogBounds.width - 60, 35};
+    if (GuiDropdownBox(dropdownTypeBounds, "Router;Switch;Access Point;Server;NAS;Printer;Camera;Sensor;UPS", activeType, editModes[2]))
+    {
+        editModes[2] = !editModes[2];
+    }
+    if (editModes[7])
+    {
+        EndScissorMode();
+    }
+    Rectangle dropdownStatusBounds = {dialogBounds.x + 20, dialogBounds.y + 40 + (7 * 45) + scrollDialog.y, dialogBounds.width - 60, 35};
+    if (!editModes[2])
+    {
+        if (GuiDropdownBox(dropdownStatusBounds, "Operacional;Em Manutenção; Em Falha; Desativado", activeStatus, editModes[7]))
+        {
+            editModes[7] = !editModes[7];
+        }
+    }
+    if (!editModes[2] || !editModes[7])
+    {
+        EndScissorMode();
     }
 }
 void drawInventory(float fontSize, float paddingAccountingForIcon, float iconSize, float iconScale, int AddIconId, int MinusIconId)
@@ -121,11 +251,11 @@ void drawInventory(float fontSize, float paddingAccountingForIcon, float iconSiz
     }
 
     Rectangle AddIconRect = {iconSize + paddingAccountingForIcon + GetScreenWidth() - paddingAccountingForIcon * 3 + 5, paddingAccountingForIcon + iconSize + (iconScale == 1 ? 4 : -4), iconScale == 1 ? 16 : iconSize, iconScale == 1 ? 16 : iconSize};
-    if (showAddItemDialog)
+    if (showAddItemDialog && !isEditing)
     {
         GuiDrawIcon(MinusIconId, AddIconRect.x, AddIconRect.y, iconScale == 1 ? 1 : 2, BLACK);
     }
-    else
+    else if (!showAddItemDialog && !isEditing)
     {
         GuiDrawIcon(AddIconId, AddIconRect.x, AddIconRect.y, iconScale == 1 ? 1 : 2, BLACK);
     }
@@ -158,6 +288,11 @@ void drawInventory(float fontSize, float paddingAccountingForIcon, float iconSiz
 
         if (GuiButton(modifyButtonBounds, "Modificar"))
         {
+            isEditing = true;
+            nodeBeingEdited = current;
+
+            MactiveType = current->item.type;
+            MactiveStatus = current->item.status;
         }
 
         if (GuiButton(deleteButtonBounds, "Deletar"))
@@ -186,103 +321,12 @@ void drawInventory(float fontSize, float paddingAccountingForIcon, float iconSiz
     }
     EndScissorMode();
 
-    if (showAddItemDialog)
+    if (showAddItemDialog && !isEditing)
     {
-        Rectangle dialogBounds = {GetScreenWidth() / 4, GetScreenHeight() / 4, GetScreenWidth() / 2, GetScreenHeight() / 2};
-        Rectangle contentDialog = {0, 0, dialogBounds.width - 20, dialogBounds.height < 355 ? 500 : dialogBounds.height - 32};
-        GuiScrollPanel(dialogBounds, "Adicionar Item", contentDialog, &scrollDialog, &viewDialog);
-
-        BeginScissorMode(dialogBounds.x, dialogBounds.y + 24, dialogBounds.width, dialogBounds.height - 32);
-
-        const char *placeholders[7] = {"Nome", "Marca", "Modelo", "IP", "MAC", "Localização", "Status"};
-
-        for (int i = 0; i < 10; i++)
-        {
-            Rectangle textBoxBounds = {dialogBounds.x + 20, dialogBounds.y + 40 + (i * 45) + scrollDialog.y, dialogBounds.width - 60, 35};
-
-            switch (i)
-            {
-            case 0:
-                drawTextBoxWithPlaceholder(textBoxBounds, itemName, 50, &editModes[0], placeholders[0]);
-                break;
-            case 1:
-                drawTextBoxWithPlaceholder(textBoxBounds, itemBrand, 50, &editModes[1], placeholders[1]);
-                break;
-            case 2:
-                break;
-            case 3:
-                if (editModes[2])
-                {
-                    break;
-                }
-                drawTextBoxWithPlaceholder(textBoxBounds, itemModel, 50, &editModes[3], placeholders[2]);
-                break;
-            case 4:
-                if (editModes[2])
-                {
-                    break;
-                }
-                drawTextBoxWithPlaceholder(textBoxBounds, itemIp, 16, &editModes[4], placeholders[3]);
-                break;
-            case 5:
-                if (editModes[2])
-                {
-                    break;
-                }
-                drawTextBoxWithPlaceholder(textBoxBounds, itemMac, 18, &editModes[5], placeholders[4]);
-                break;
-            case 6:
-                if (editModes[2])
-                {
-                    break;
-                }
-                drawTextBoxWithPlaceholder(textBoxBounds, itemLocation, 100, &editModes[6], placeholders[5]);
-                break;
-            case 7:
-                break;
-            case 8:
-                if (editModes[2] || editModes[7])
-                {
-                    break;
-                }
-                drawTextBoxWithPlaceholder(textBoxBounds, itemStatus, 20, &editModes[8], placeholders[6]);
-                break;
-            case 9:
-                if (editModes[2] || editModes[7])
-                {
-                    break;
-                }
-                if (GuiLabelButton(textBoxBounds, "Adicionar Item"))
-                {
-                    saveItem();
-                }
-                break;
-            }
-        }
-        if (editModes[2])
-        {
-            EndScissorMode();
-        }
-        Rectangle dropdownTypeBounds = {dialogBounds.x + 20, dialogBounds.y + 40 + (2 * 45) + scrollDialog.y, dialogBounds.width - 60, 35};
-        if (GuiDropdownBox(dropdownTypeBounds, "Router;Switch;Access Point;Server;NAS;Printer;Camera;Sensor;UPS", &activeItems[0], editModes[2]))
-        {
-            editModes[2] = !editModes[2];
-        }
-        if (editModes[7])
-        {
-            EndScissorMode();
-        }
-        Rectangle dropdownStatusBounds = {dialogBounds.x + 20, dialogBounds.y + 40 + (7 * 45) + scrollDialog.y, dialogBounds.width - 60, 35};
-        if (!editModes[2])
-        {
-            if (GuiDropdownBox(dropdownStatusBounds, "Operacional;Em Manutenção; Em Falha; Desativado", &activeItems[1], editModes[7]))
-            {
-                editModes[7] = !editModes[7];
-            }
-        }
-        if (!editModes[2] || !editModes[7])
-        {
-            EndScissorMode();
-        }
+        showModal(itemName, itemBrand, itemModel, itemIp, itemMac, itemLocation, itemLastMaintenanceDate, &activeType, &activeStatus);
+    }
+    if (!showAddItemDialog && isEditing)
+    {
+        showModal(nodeBeingEdited->item.name, nodeBeingEdited->item.brand, nodeBeingEdited->item.model, nodeBeingEdited->item.ipAddress, nodeBeingEdited->item.macAddress, nodeBeingEdited->item.location, nodeBeingEdited->item.lastMaintenanceDate, &MactiveType, &MactiveStatus);
     }
 }
